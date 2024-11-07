bb56b7ef8f0afeb7fb2193bf11309929f3a87040
Francisco Javier Martín Fernández <energy.rex.2003@gmail.com>
2024-11-08 00:35:46 +0100

    Actualizado para citas

--- view/citas.php
+++ view/citas.php
@@ -7,7 +7,8 @@
   <title>Document</title>
   <link rel="preconnect" href="https://rsms.me/">
   <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-  <link rel="stylesheet" href="./index.css">
+  <link rel="stylesheet" href="./main.css">
+  <script src=<?php echo FA6_URL ?> crossorigin="anonymous"></script>
 <style>
   body {
     font-family: 'Inter', sans-serif;
@@ -15,42 +16,32 @@
 </style>
 </head>
 <body>
+<?php 
+  require "./componentes/modals.php";
+  renderModalInsert();
+  renderModalUpdate();
+  renderDeleteModal();
+?>
   <h1>Citas</h1>
   <?php require './componentes/sidebar.php' ?>
-  
-  <div><input type="text"> <button>Buscar</button> <button>Filtro</button><button>Insertar paciente</button></div>
+
+
+  <div><input type="text"> <button>Buscar</button> <button>Filtro</button> <button class="insert__btn">+</button> </div>
   <table>
     <tr>
+      <th>ID Cita</th>
       <th>Paciente</th>
       <th>Doctor</th>
       <th>Estado</th>
       <th>Motivo</th>
       <th>Día</th>
       <th>Hora</th>
+      <th>Modificar</th>
+      <th>Eliminar</th>
     </tr>
     <?php mostrarCitas(); ?>
   </table>
 
-  <div class="dialog__insert">
-    <h1>Insertar un nuevo dato.</h1>
-    <form action="./citas.php" method="post">
-      <label for="paciente">Paciente</label>
-      <input type="text" name="paciente">
-      <label for="doctor">Doctor</label>
-      <input type="text" name="doctor">
-      <label for="estado">Estado</label>
-      <input type="text" name="estado">
-      <label for="motivo">Motivo</label>
-      <input type="text" name="paciente">
-      <label for="dia">Día</label>
-      <input type="date" name="paciente">
-      <label for="hora">Hora</label>
-      <input type="datetime" name="hora">
-      <button type="submit">Insertar</button>
-
-    </form>
-  </div>
-
-  <?php require './componentes/footer.php' ?>
+  <script src="citas.js"></script>
 </body>
 </html>
