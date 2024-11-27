f6cfb0c98bae481ac421f964dbff868fc561e94d
EnergyREX <energy.rex.2003@gmail.com>
2024-11-28 00:15:55 +0100

    Remake of database

--- BBDD/seed.sql
+++ BBDD/seed.sql
@@ -1,337 +1,103 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 19-11-2024 a las 10:50:15
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE DATABASE clinic;
+USE clinic;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE `appointments` (
+  `id` int(255) NOT NULL,
+  `patient_dni` varchar(9) NOT NULL,
+  `doctor_dni` varchar(9) NOT NULL,
+  `hour` date NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Base de datos: `clinica`
---
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `last_updated` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE DATABASE clinica;
-USE clinica;
+CREATE TABLE `medical_records` (
+  `id` int(11) NOT NULL,
+  `patient_dni` varchar(9) NOT NULL,
+  `record_date` date NOT NULL,
+  `details` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Estructura de tabla para la tabla `admin`
---
+CREATE TABLE `treatments` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `admin` (
+CREATE TABLE `users` (
   `pfpimg` varchar(255) DEFAULT NULL,
   `dni` varchar(9) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `apellidos` varchar(45) NOT NULL,
-  `direccion` varchar(255) NOT NULL,
-  `telefono` varchar(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
-  `especialidad` varchar(50) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone_number` varchar(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `passwd` varchar(255) NOT NULL,
+  `specialization` int(20) DEFAULT NULL,
   `fecha_union` date NOT NULL,
-  `disponibilidad` varchar(45) NOT NULL
+  `disponibilidad` varchar(255) DEFAULT NULL,
+  `state` varchar(255) NOT NULL,
+  `role_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `admin`
---
-
-INSERT INTO `admin` (`pfpimg`, `dni`, `nombre`, `apellidos`, `direccion`, `telefono`, `email`, `contraseña`, `especialidad`, `fecha_union`, `disponibilidad`) VALUES
-('0', '12345678A', 'Juan', 'Pérez', 'Calle Falsa 123', '612345678', 'juan.perez@clinica.com', 'password123', 'Cardiología', '2024-01-15', 'Lunes a Viernes 9:00-18:00'),
-('0', '22345678B', 'María', 'López', 'Avenida Gran Vía 21', '612345679', 'maria.lopez@clinica.com', 'password456', 'Dermatología', '2024-02-25', 'Lunes a Viernes 8:00-14:00'),
-('0', '32345678C', 'Carlos', 'Rodríguez', 'Calle del Río 88', '622345680', 'carlos.rodriguez@clinica.com', 'password789', 'Neurología', '2024-03-18', 'Martes a Jueves 9:00-17:00'),
-('0', '42345678D', 'Lucía', 'Torres', 'Calle de la Paz 43', '632345681', 'lucia.torres@clinica.com', 'password012', 'Endocrinología', '2024-04-02', 'Lunes a Viernes 10:00-19:00');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `asistente`
---
-
-CREATE TABLE `asistente` (
-  `pfpimg` varchar(255) DEFAULT NULL,
-  `dni` varchar(9) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `apellidos` varchar(45) NOT NULL,
-  `direccion` varchar(255) NOT NULL,
-  `telefono` varchar(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
-  `fecha_union` date NOT NULL,
-  `disponibilidad` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `asistente`
---
-
-INSERT INTO `asistente` (`pfpimg`, `dni`, `nombre`, `apellidos`, `direccion`, `telefono`, `email`, `contraseña`, `fecha_union`, `disponibilidad`) VALUES
-(NULL, '33456789C', 'Luis', 'Pérez', 'Calle del Sol 89', '632345679', 'luis.perez@clinica.com', 'password789', '2024-03-30', 'Lunes a Viernes 9:00-14:00'),
-(NULL, '43456789D', 'Elena', 'Vega', 'Calle Santa Teresa 150', '642345680', 'elena.vega@clinica.com', 'password321', '2024-04-12', 'Martes a Sábado 10:00-17:00'),
-(NULL, '53456789E', 'Jorge', 'García', 'Calle de la Luz 10', '652345681', 'jorge.garcia@clinica.com', 'password654', '2024-05-05', 'Lunes a Viernes 8:00-15:00');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `citas`
---
-
-CREATE TABLE `citas` (
-  `id` int(10) NOT NULL,
-  `paciente` varchar(9) NOT NULL,
-  `doctor` varchar(9) NOT NULL,
-  `estado` varchar(45) NOT NULL,
-  `motivo` text NOT NULL,
-  `dia` date NOT NULL,
-  `hora` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `citas`
---
-
-INSERT INTO `citas` (`id`, `paciente`, `doctor`, `estado`, `motivo`, `dia`, `hora`) VALUES
-(33, '12345678T', '54567892E', 'Cancelada', 'Lobotomía', '2024-01-01', '00:00:00'),
-(34, '12345678T', '55071363T', 'Cancelada', 'Lobotomía', '2024-01-01', '00:00:00'),
-(35, '12345678T', '54567892E', 'Confirmada', 'Vacunación', '2024-01-01', '00:00:00');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `doctores`
---
-
-CREATE TABLE `doctores` (
-  `pfpimg` varchar(255) DEFAULT NULL,
-  `dni` varchar(9) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `apellidos` varchar(45) NOT NULL,
-  `direccion` varchar(255) NOT NULL,
-  `telefono` varchar(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
-  `especialidad` varchar(50) NOT NULL,
-  `fecha_union` date NOT NULL,
-  `disponibilidad` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `doctores`
---
-
-INSERT INTO `doctores` (`pfpimg`, `dni`, `nombre`, `apellidos`, `direccion`, `telefono`, `email`, `contraseña`, `especialidad`, `fecha_union`, `disponibilidad`) VALUES
-(NULL, '54567892E', 'Antonio', 'Jiménez', 'Calle Sevilla 101', '652345682', 'antonio.jimenez@clinica.com', 'password123', 'Traumatología', '2024-05-20', 'Lunes a Viernes 9:00-18:00'),
-(NULL, '55071363T', 'Francisco Javier', 'Martín Fernández', 'Calle Aristóteles 11', '617069901', 'mrenergyrex@gmail.com', 'admin', 'Comer gatos', '2024-01-01', 'Siempre'),
-(NULL, '64567893F', 'Raquel', 'Alonso', 'Calle Toledo 33', '662345683', 'raquel.alonso@clinica.com', 'password567', 'Oftalmología', '2024-06-01', 'Martes a Viernes 10:00-16:00');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `paciente`
---
-
-CREATE TABLE `paciente` (
-  `pfpimg` varchar(255) DEFAULT NULL,
-  `dni` varchar(9) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `apellidos` varchar(45) NOT NULL,
-  `direccion` varchar(255) NOT NULL,
-  `telefono` varchar(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `contraseña` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `paciente`
---
-
-INSERT INTO `paciente` (`pfpimg`, `dni`, `nombre`, `apellidos`, `direccion`, `telefono`, `email`, `contraseña`) VALUES
-(NULL, '12345678T', 'Francisco Javier', 'Martín Fernández', 'Calle Aristóteles 11', '617069901', 'admin@admin', ''),
-(NULL, '45678901D', 'Lucía', 'Fernández', 'Calle Luna 101', '642345678', 'lucia.fernandez@paciente.com', 'password101'),
-(NULL, '55071361T', 'Francisco Javier', 'Martín Fernández', 'Calle Aristóteles 11', '617069901', 'admin@admin', 'password404');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `presupuestos`
---
-
-CREATE TABLE `presupuestos` (
-  `id` int(10) NOT NULL,
-  `paciente` varchar(9) NOT NULL,
-  `doctor` varchar(9) NOT NULL,
-  `tratamiento` int(11) NOT NULL,
-  `precio` decimal(11,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `presupuestos`
---
-
-INSERT INTO `presupuestos` (`id`, `paciente`, `doctor`, `tratamiento`, `precio`) VALUES
-(4, '55071361T', '54567892E', 4, 350);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tratamientos`
---
-
-CREATE TABLE `tratamientos` (
-  `id` int(10) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `precio` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `tratamientos`
---
-
-INSERT INTO `tratamientos` (`id`, `nombre`, `precio`) VALUES
-(1, 'Consulta pediátrica', 150),
-(2, 'Revisión ginecológica', 120),
-(3, 'Consulta general', 180),
-(4, 'Vacunación', 50),
-(5, 'Tratamiento dental', 200),
-(6, 'Revisión traumatológica', 300),
-(7, 'Consulta oftalmológica', 100);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tratamientos_presupuestos`
---
-
-CREATE TABLE `tratamientos_presupuestos` (
-  `presupuesto_id` int(10) NOT NULL,
-  `tratamiento_id` int(10) NOT NULL,
-  `cantidad` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `tratamientos_presupuestos`
---
-
-INSERT INTO `tratamientos_presupuestos` (`presupuesto_id`, `tratamiento_id`, `cantidad`) VALUES
-(4, 6, 1);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`dni`);
-
---
--- Indices de la tabla `asistente`
---
-ALTER TABLE `asistente`
-  ADD PRIMARY KEY (`dni`);
-
---
--- Indices de la tabla `citas`
---
-ALTER TABLE `citas`
+ALTER TABLE `appointments`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `dni_doctor` (`doctor`),
-  ADD KEY `dni_paciente` (`paciente`);
+  ADD KEY `dni_doctor` (`doctor_dni`),
+  ADD KEY `dni_paciente` (`patient_dni`);
 
---
--- Indices de la tabla `doctores`
---
-ALTER TABLE `doctores`
-  ADD PRIMARY KEY (`dni`);
-
---
--- Indices de la tabla `paciente`
---
-ALTER TABLE `paciente`
-  ADD PRIMARY KEY (`dni`);
-
---
--- Indices de la tabla `presupuestos`
---
-ALTER TABLE `presupuestos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `paciente_dni` (`paciente`),
-  ADD KEY `doctor_dni` (`doctor`);
-
---
--- Indices de la tabla `tratamientos`
---
-ALTER TABLE `tratamientos`
+ALTER TABLE `inventory`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `tratamientos_presupuestos`
---
-ALTER TABLE `tratamientos_presupuestos`
-  ADD KEY `id_presupuesto` (`presupuesto_id`),
-  ADD KEY `id_tratamiento` (`tratamiento_id`);
+ALTER TABLE `medical_records`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `paciente_dni` (`patient_dni`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de la tabla `citas`
---
-ALTER TABLE `citas`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+ALTER TABLE `treatments`
+  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de la tabla `presupuestos`
---
-ALTER TABLE `presupuestos`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`dni`),
+  ADD KEY `rol` (`role_id`);
 
---
--- AUTO_INCREMENT de la tabla `tratamientos`
---
-ALTER TABLE `tratamientos`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `appointments`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
---
--- Restricciones para tablas volcadas
---
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Filtros para la tabla `citas`
---
-ALTER TABLE `citas`
-  ADD CONSTRAINT `dni_doctor` FOREIGN KEY (`doctor`) REFERENCES `doctores` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `dni_paciente` FOREIGN KEY (`paciente`) REFERENCES `paciente` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `medical_records`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Filtros para la tabla `presupuestos`
---
-ALTER TABLE `presupuestos`
-  ADD CONSTRAINT `doctor_dni` FOREIGN KEY (`doctor`) REFERENCES `doctores` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `paciente_dni` FOREIGN KEY (`paciente`) REFERENCES `paciente` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Filtros para la tabla `tratamientos_presupuestos`
---
-ALTER TABLE `tratamientos_presupuestos`
-  ADD CONSTRAINT `id_presupuesto` FOREIGN KEY (`presupuesto_id`) REFERENCES `presupuestos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `id_tratamiento` FOREIGN KEY (`tratamiento_id`) REFERENCES `tratamientos` (`id`);
+ALTER TABLE `treatments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `dni_doctor` FOREIGN KEY (`doctor_dni`) REFERENCES `users` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `dni_paciente` FOREIGN KEY (`patient_dni`) REFERENCES `users` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `medical_records`
+  ADD CONSTRAINT `paciente_dni` FOREIGN KEY (`patient_dni`) REFERENCES `users` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `users`
+  ADD CONSTRAINT `rol` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
